7aba8c049e05f3fcb956fee0d09a8302a290693a
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 00:02:09 +0530

    Create Last Index of Number.java

---
+++ 2.Data_Structures_in_Java/Lec3_Recursion1/Last Index of Number.java
@@ -0,0 +1,56 @@
+/*
+Given an array of length N and an integer x, you need to find and return the last index of integer x present in the array. Return -1 if it is not present in the array.
+Last index means - if x is present multiple times in the array, return the index at which x comes last in the array.
+You should start traversing your array from 0, not from (N - 1).
+Do this recursively. Indexing in the array starts from 0.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+last index or -1
+Constraints :
+1 <= N <= 10^3
+Sample Input :
+4
+9 8 10 8
+8
+Sample Output :
+3
+*/
+
+public class Solution {
+
+	public static int lastIndex(int input[], int x) {
+        return lastIndex(input,x,input.length-1);
+    }
+    private static int lastIndex(int input[], int x,int lastIndex) {
+        if(lastIndex<0)
+            return -1;
+        if(input[lastIndex]==x)
+            return lastIndex;
+		return lastIndex(input,x,lastIndex-1);
+	}
+	
+}
+import java.util.Scanner;
+
+public class Runner {
+	
+	static Scanner s = new Scanner(System.in);
+	
+	public static int[] takeInput(){
+		int size = s.nextInt();
+		int[] input = new int[size];
+		for(int i = 0; i < size; i++){
+			input[i] = s.nextInt();
+		}
+		return input;
+	}
+	
+	public static void main(String[] args) {
+		int[] input = takeInput();
+		int x = s.nextInt();
+		System.out.println(Solution.lastIndex(input, x));
+	}
+}

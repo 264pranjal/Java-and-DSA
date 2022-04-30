cb8dc6a1a972213f8bc14855a5cec641adcbfe5e
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 01:15:47 +0530

    Create Bubble Sort (Iterative) LinkedList.java

---
+++ 2.Data_Structures_in_Java/Lec9_Linked_List_2/Bubble Sort (Iterative) LinkedList.java
@@ -0,0 +1,123 @@
+/*
+Given a singly linked list of integers, sort it using 'Bubble Sort.'
+Note :
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format :
+The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format :
+For each test case/query, print the elements of the sorted singly linked list.
+
+Output for every test case will be printed in a seperate line.
+Constraints :
+0 <= M <= 10^3
+Where M is the size of the singly linked list.
+
+Time Limit: 1sec
+Sample Input 1 :
+10 9 8 7 6 5 4 3 -1
+Sample Output 1 :
+ 3 4 5 6 7 8 9 10 
+ Sample Output 2 :
+10 -5 9 90 5 67 1 89 -1
+Sample Output 2 :
+-5 1 5 9 10 67 89 90 
+*/
+public class Solution {
+
+	public static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
+		if(head==null || head.next==null)
+            return head;
+        for(int i=0;i<length(head)-1;i++){
+            LinkedListNode<Integer> prev = null;
+            LinkedListNode<Integer> curr = head;
+            LinkedListNode<Integer> next = curr.next;
+
+            while(curr.next != null){
+                if(curr.data > curr.next.data){
+                    if(prev == null){
+                        curr.next = next.next;
+                        next.next = curr;
+                        prev = next;
+                        head = prev;
+                    }else{
+                        next = curr.next;
+                        curr.next = next.next;
+                        prev.next = next;
+                        next.next = curr;
+                        prev = next;
+                    }
+                }else{
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+        }
+        return head;
+	}
+    public static int length(LinkedListNode<Integer> head){
+        int count=0;
+        while(head!=null){
+            count++;
+            head=head.next;
+        }
+        return count;
+    }
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	LinkedListNode<Integer> head = takeInput(); 
+
+    	head = Solution.bubbleSort(head);
+    	print(head);
+    }
+}

c2330c6145dbe9c4302b41d1c8234d9f5ea829f6
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-04-30 19:27:14 +0530

    Update Fahrenheit to Celsius Table.java

--- 1.Introduction_to_Java/Lec8_functions_variables_scope/Fahrenheit to Celsius Table.java
+++ 1.Introduction_to_Java/Lec8_functions_variables_scope/Fahrenheit to Celsius Table.java
@@ -45,3 +45,15 @@ public class Solution {
 		
 	}
 }
+import java.util.Scanner;
+
+public class Runner{
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int start = s.nextInt();
+		int end = s.nextInt();
+		int step = s.nextInt();
+		Solution.printFahrenheitTable(start, end, step);
+	}
+}

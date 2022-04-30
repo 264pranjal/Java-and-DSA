17b12d8bf4fef475acebf309514f94b933c9f49e
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 00:03:26 +0530

    Create Multiplication (Recursive).java

---
+++ 2.Data_Structures_in_Java/Lec3_Recursion1/Multiplication (Recursive).java
@@ -0,0 +1,42 @@
+/*
+Given two integers M & N, calculate and return their multiplication using recursion. You can only use subtraction and addition for your calculation. No other operators are allowed.
+Input format :
+Line 1 : Integer M
+Line 2 : Integer N
+Output format :
+M x N
+Constraints :
+0 <= M <= 1000
+0 <= N <= 1000
+Sample Input 1 :
+3 
+5
+Sample Output 1 :
+15
+Sample Input 2 :
+4 
+0
+Sample Output 2 :
+0
+*/
+
+
+public class solution {
+
+	public static int multiplyTwoIntegers(int m, int n){
+        if(n==0)
+            return 0;
+		return m+multiplyTwoIntegers(m,n-1);
+	}
+}
+
+import java.util.Scanner;
+
+public class runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		int n = s.nextInt();
+		System.out.println(solution.multiplyTwoIntegers(m, n));
+	}
+}

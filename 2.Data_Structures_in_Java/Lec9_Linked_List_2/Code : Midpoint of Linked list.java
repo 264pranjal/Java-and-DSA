7379c6aba431ff728bfbb3c99e9741c33f0b3aec
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 01:07:25 +0530

    Create Code : Midpoint of Linked list.java

---
+++ 2.Data_Structures_in_Java/Lec9_Linked_List_2/Code : Midpoint of Linked list.java
@@ -0,0 +1,116 @@
+/*
+For a given singly linked list of integers, find and return the node present at the middle of the list.
+Note :
+If the length of the singly linked list is even, then return the first middle node.
+
+Example: Consider, 10 -> 20 -> 30 -> 40 is the given list, then the nodes present at the middle with respective data values are, 20 and 30. We return the first node with data 20.
+ Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space. 
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+ Output Format :
+For each test case/query, print the data value of the node at the middle of the given list.
+
+Output for every test case will be printed in a seperate line.
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5 
+Where M is the size of the singly linked list.
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+1 2 3 4 5 -1
+Sample Output 1 :
+3
+Sample Input 2 :
+2 
+-1
+1 2 3 4 -1
+Sample Output 2 :
+2
+*/
+public class Solution {
+    
+    public static LinkedListNode<Integer> midPoint(LinkedListNode<Integer> head) {
+        if(head==null || head.next == null)
+            return head;
+        LinkedListNode<Integer> slow=head,fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput(); 
+
+            LinkedListNode<Integer> mid = Solution.midPoint(head);
+            
+            if (mid != null) {
+                System.out.println(mid.data);
+            }
+
+            t -= 1;
+        }
+
+    }
+}

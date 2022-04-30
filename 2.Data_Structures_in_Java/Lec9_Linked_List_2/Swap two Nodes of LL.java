00748d62b7f0d29092e25e0eb3804506c0040a79
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 01:14:21 +0530

    Create Swap two Nodes of LL.java

---
+++ 2.Data_Structures_in_Java/Lec9_Linked_List_2/Swap two Nodes of LL.java
@@ -0,0 +1,140 @@
+/*
+You have been given a singly linked list of integers along with two integers, 'i,' and 'j.' Swap the nodes that are present at the 'i-th' and 'j-th' positions.
+Note :
+Remember, the nodes themselves must be swapped and not the datas.
+
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains the elements of the singly linked list separated by a single space.
+
+The second line of input contains two integer values 'i,' and 'j,' respectively. A single space will separate them.
+ Remember/consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format :
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a seperate line.
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+0 <= i < M
+0 <= j < M
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+3 4
+Sample Output 1 :
+3 4 5 6 2 1 9 
+ Sample Input 2 :
+2
+10 20 30 40 -1
+1 2
+70 80 90 25 65 85 90 -1
+0 6
+ Sample Output 2 :
+10 30 20 40 
+90 80 90 25 65 85 70 
+*/
+
+public class Solution {
+
+	public static LinkedListNode<Integer> swapNodes(LinkedListNode<Integer> head, int i, int j) {
+		if(head==null || head.next==null)
+            return head;
+        int count=0;
+        int t1=0,t2=0,t3=0;
+        LinkedListNode<Integer> temp1=null,temp2=null,temp3=head,temp4=null;
+        while(temp3!=null){
+            if(count==i){
+                t1=temp3.data;
+                temp1=temp3;
+            }
+            if(count==j){
+                t2=temp3.data;
+                temp2=temp3;
+            }
+            count++;
+            temp3=temp3.next;
+        }
+        if(temp1!=null && temp2!=null){
+            temp1.data=t2;
+            temp2.data=t1;
+        }
+        return head;
+	}
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+            
+            LinkedListNode<Integer> head = takeInput();
+            String[] i_j = br.readLine().trim().split("\\s");
+
+            int i = Integer.parseInt(i_j[0]);
+            int j = Integer.parseInt(i_j[1]);
+
+            LinkedListNode<Integer> newHead = Solution.swapNodes(head, i, j);
+            print(newHead);
+            
+            t -= 1;
+        }
+
+    }
+}

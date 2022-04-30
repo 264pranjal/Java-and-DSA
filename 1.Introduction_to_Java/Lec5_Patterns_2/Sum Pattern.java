d94d60831975d1fe0478f7b30ef0591139a29e5b
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-04-30 15:25:28 +0530

    Create Sum Pattern.java

---
+++ 1.Introduction_to_Java/Lec5_Patterns_2/Sum Pattern.java
@@ -0,0 +1,45 @@
+/*
+Write a program to print triangle of user defined integers sum.
+Input Format :
+A single integer, N
+Output Format :
+Required Pattern
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+1=1
+1+2=3
+1+2+3=6
+Sample Input 2 :
+ 5
+Sample Output 2 :
+1=1
+1+2=3
+1+2+3=6
+1+2+3+4=10
+1+2+3+4+5=15
+*/
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++) {
+			int sum=0;
+			System.out.print(1);
+			for(int j=2;j<=i;j++) {
+				System.out.print("+" + j);
+			}
+			System.out.print("=");
+			for(int j=1;j<=i;j++) {
+				sum=sum+j;
+			}
+			System.out.print(sum);
+			System.out.println();
+		}
+	}
+
+}

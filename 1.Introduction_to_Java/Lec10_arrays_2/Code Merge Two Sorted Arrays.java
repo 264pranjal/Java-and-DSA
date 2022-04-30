1ec4c074adb56c88df98ad2a965d7b834be5e8ab
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-04-30 20:44:03 +0530

    Create Code Merge Two Sorted Arrays.java

---
+++ 1.Introduction_to_Java/Lec10_arrays_2/Code Merge Two Sorted Arrays.java
@@ -0,0 +1,115 @@
+/*
+You have been given two sorted arrays/lists(ARR1 and ARR2) of size N and M respectively, merge them into a third array/list such that the third array is also sorted.
+Input Format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+
+Second line contains 'N' single space separated integers representing the elements of the first array/list.
+
+Third line contains an integer 'M' representing the size of the second array/list.
+
+Fourth line contains 'M' single space separated integers representing the elements of the second array/list.
+Output Format :
+For each test case, print the sorted array/list(of size N + M) in a single row, separated by a single space.
+
+Output for every test case will be printed in a separate line.
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+0 <= M <= 10^5
+Time Limit: 1 sec 
+Sample Input 1 :
+1
+5
+1 3 4 7 11
+4
+2 4 6 13
+Sample Output 1 :
+1 2 3 4 4 6 7 11 13 
+Sample Input 2 :
+2
+3
+10 100 500
+7
+4 7 9 25 30 300 450
+4
+7 45 89 90
+0
+Sample Output 2 :
+4 7 9 10 25 30 100 300 450 500
+7 45 89 90
+*/
+public class Solution {  
+
+    public static int[] merge(int arr1[], int arr2[]) {
+    	int l=arr1.length+arr2.length;
+        int arr3[]=new int[l];
+        int i=0,j=0,k=0;
+        while(j<arr1.length && k<arr2.length){
+            if(arr1[j]<arr2[k]){
+                arr3[i++]=arr1[j++];
+            }
+            else{
+               arr3[i++]=arr2[k++];
+            }
+      }
+        while(j<arr1.length){
+            arr3[i++]=arr1[j++];
+        }
+        while(k<arr2.length){
+            arr3[i++]=arr2[k++];
+        }
+        return arr3;
+    }
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+        
+        String[] strNums; 
+        strNums = br.readLine().split("\\s");
+        
+
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[] input1 = takeInput();
+            int[] input2 = takeInput();
+            int[] ans = Solution.merge(input1, input2);
+            printArray(ans);
+
+            t -= 1;
+        }
+    }
+}

65679cf48f9398d8ba9157ee5bef6536ebf73d59
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 01:03:35 +0530

    Create Palindrome LinkedList.java

---
+++ 2.Data_Structures_in_Java/Lec8_Linked_List_1/Palindrome LinkedList.java
@@ -0,0 +1,135 @@
+/*
+You have been given a head to a singly linked list of integers. Write a function check to whether the list given is a 'Palindrome' or not.
+ Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First and the only line of each test case or query contains the the elements of the singly linked list separated by a single space.
+ Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+ Output format :
+For each test case, the only line of output that print 'true' if the list is Palindrome or 'false' otherwise.
+ Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Time Limit: 1sec
+
+Where 'M' is the size of the singly linked list.
+Sample Input 1 :
+1
+9 2 3 3 2 9 -1
+Sample Output 1 :
+true
+Sample Input 2 :
+2
+0 2 3 2 5 -1
+-1
+Sample Output 2 :
+false
+true
+Explanation for the Sample Input 2 :
+For the first query, it is pretty intuitive that the the given list is not a palindrome, hence the output is 'false'.
+
+For the second query, the list is empty. An empty list is always a palindrome , hence the output is 'true'.
+*/
+public class Solution {
+
+	public static boolean isPalindrome(LinkedListNode<Integer> head) {
+		if(head==null||head.next==null) return true;
+
+        LinkedListNode<Integer> slow = head;
+        LinkedListNode<Integer> fast = head;
+
+        while(fast.next!=null&&fast.next.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow.next = reverse(slow.next);
+        slow = slow.next;
+        LinkedListNode<Integer> dummy = head;
+
+        while(slow!=null) {
+            if(dummy.data != slow.data) return false;
+            dummy = dummy.next;
+            slow = slow.next;
+        }
+        return true;
+	}
+    public static LinkedListNode<Integer> reverse(LinkedListNode<Integer> ptr) {
+        LinkedListNode<Integer> pre=null;
+        LinkedListNode<Integer> nex=null;
+        while(ptr!=null) {
+            nex = ptr.next;
+            ptr.next = pre;
+            pre=ptr;
+            ptr=nex;
+        }
+        return pre;
+    }
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput(); 
+
+            boolean ans = Solution.isPalindrome(head);
+            System.out.println(ans);
+
+            t -= 1;
+
+        }
+        
+    }
+}

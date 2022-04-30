21216e4f21a93e8bc3e400d04480c44206a70edc
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 01:11:58 +0530

    Create Find a node in LL (recursive).java

---
+++ 2.Data_Structures_in_Java/Lec9_Linked_List_2/Find a node in LL (recursive).java
@@ -0,0 +1,121 @@
+/*
+Given a singly linked list of integers and an integer n, find and return the index for the first occurrence of 'n' in the linked list. -1 otherwise.
+Follow a recursive approach to solve this.
+Note :
+Assume that the Indexing for the linked list always starts from 0.
+ Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains the elements of the singly linked list separated by a single space.
+
+The second line of input contains a single integer depicting the value of 'n'.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+ Output format :
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a seperate line.
+ Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+
+Time Limit:  1sec
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+100
+Sample Output 1 :
+-1
+Sample Input 2 :
+2
+10 20010 30 400 600 -1
+20010
+100 200 300 400 9000 -34 -1
+-34
+Sample Output 2 :
+1
+5
+*/
+public class Solution {
+
+
+	public static int findNodeRec(LinkedListNode<Integer> head, int n) {
+        if(head==null)
+            return -1;
+        return findNodeRec(head,n,0);
+	}
+    public static int findNodeRec(LinkedListNode<Integer> head, int n,int pos) {
+        if(head==null)
+            return -1;
+        if(head.data==n)
+            return pos;
+        return findNodeRec(head.next,n,pos+1);
+	}
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head) {
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+            
+            LinkedListNode<Integer> head = takeInput();
+            int n = Integer.parseInt(br.readLine().trim());
+
+            System.out.println(Solution.findNodeRec(head, n));
+            
+            t -= 1;
+        }
+
+    }
+}

e7417c796f741030c516d408c873330c4bbf7720
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 00:32:15 +0530

    Create Triplet sum.java

---
+++ 2.Data_Structures_in_Java/Lec5_Time_Complexity/Triplet sum.java
@@ -0,0 +1,137 @@
+/*
+You have been given a random integer array/list(ARR) and a number X. Find and return the triplet(s) in the array/list which sum to X.
+Note :
+Given array/list can contain duplicate elements.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+Third line contains an integer 'X'.
+Output format :
+For each test case, print the total number of triplets present in the array/list.
+
+Output for every test case will be printed in a separate line.
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^3
+0 <= X <= 10^9
+
+Time Limit: 1 sec
+Sample Input 1:
+1
+7
+1 2 3 4 5 6 7 
+12
+Sample Output 1:
+5
+Sample Input 2:
+2
+7
+1 2 3 4 5 6 7 
+19
+9
+2 -5 8 -6 0 5 10 11 -3
+10
+Sample Output 2:
+0
+5
+
+
+ Explanation for Input 2:
+Since there doesn't exist any triplet with sum equal to 19 for the first query, we print 0.
+
+For the second query, we have 5 triplets in total that sum up to 10. They are, (2, 8, 0), (2, 11, -3), (-5, 5, 10), (8, 5, -3) and (-6, 5, 11)
+*/
+
+import java.util.*;
+public class Solution {	
+
+    public static int tripletSum(int[] arr, int sum) {
+        Arrays.sort(arr);
+        int count=0;
+        int n=arr.length;
+        for (int i = 0;i < n - 1; i++) {
+            int l = i + 1;
+            int r = n - 1;
+            int x = arr[i];
+            while (l < r) {
+                if (x + arr[l] + arr[r] == sum) {
+                    int count1 =0, count2=0;
+                    if(arr[l] == arr[r]){
+                        count = count + ((r-l)*(r+1-l))/2;
+                        break;
+                    }
+                    else{
+                        int temp = l;
+                        while(arr[l] == arr[temp]){
+                            l++;
+                            count1++;
+                        }
+                        temp = r;
+                        while(arr[r] == arr[temp]){
+                            r--;
+                            count2++;
+                        }
+                        count += count1*count2;
+                    }
+                }
+                else if (x + arr[l] + arr[r] < sum)
+                    l++;
+                else
+                    r--;
+            }
+        }
+        return count;
+    }
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+        
+        String[] strNums; 
+        strNums = br.readLine().split("\\s");
+        
+
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[] arr = takeInput();
+            int num = Integer.parseInt(br.readLine().trim());
+            System.out.println(Solution.tripletSum(arr, num));
+
+            t -= 1;
+        }
+    }
+}

768bf1ac9fa916a4bf49e59149afd241f1d03a5c
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 00:00:16 +0530

    Create Sum of Array.java

---
+++ 2.Data_Structures_in_Java/Lec3_Recursion1/Sum of Array.java
@@ -0,0 +1,50 @@
+/*
+Given an array of length N, you need to find and return the sum of all elements of the array.
+Do this recursively.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+Sum
+Constraints :
+1 <= N <= 10^3
+Sample Input 1 :
+3
+9 8 9
+Sample Output 1 :
+26
+Sample Input 2 :
+3
+4 2 1
+Sample Output 2 :
+7    
+*/
+
+public class Solution {
+
+	public static int sum(int input[]) {
+        if(input.length==1)
+            return input[0];
+        
+        int small[]=new int[input.length-1]; 
+        for(int i=1;i<input.length;i++){
+            small[i-1]=input[i];
+        }
+        return input[0]+sum(small);
+	}
+}
+import java.util.Scanner;
+
+public class Runner {
+
+	static Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n = s.nextInt();
+		int input[] = new int[n];
+		for(int i = 0; i < n; i++) {
+			input[i] = s.nextInt();
+		}
+		System.out.println(Solution.sum(input));
+	}
+}

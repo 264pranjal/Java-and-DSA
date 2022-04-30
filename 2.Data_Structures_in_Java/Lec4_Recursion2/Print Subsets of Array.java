9b36675f0b17709412945e9de37198bd6b0e60ca
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 00:21:46 +0530

    Create Print Subsets of Array.java

---
+++ 2.Data_Structures_in_Java/Lec4_Recursion2/Print Subsets of Array.java
@@ -0,0 +1,70 @@
+/*
+Given an integer array (of length n), find and print all the subsets of input array.
+Subsets are of length varying from 0 to n, that contain elements of the array. But the order of elements should remain same as in the input array.
+Note : The order of subsets are not important. Just print the subsets in different lines.
+Input format :
+Line 1 : Integer n, Size of array
+Line 2 : Array elements (separated by space)
+Constraints :
+1 <= n <= 15
+Sample Input:
+3
+15 20 12
+Sample Output:
+[] (this just represents an empty array, don't worry about the square brackets)
+12 
+20 
+20 12 
+15 
+15 12 
+15 20 
+15 20 12 
+*/
+public class solution
+{
+    public static void printSubsets(int input[]) 
+    {
+        int output[]=new int[0];
+        printsubsets(input,0,output);
+    }
+    public static void printsubsets(int input[],int si,int output[])
+    { 
+        if(si==input.length)
+        {
+            for(int i:output){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+        printsubsets(input,si+1,output);
+       
+        int newoutput[]=new int[output.length+1];
+          int j=0;
+        for( ;j<output.length;j++)
+        {
+            newoutput[j]=output[j];
+        }
+        newoutput[j]=input[si];
+        printsubsets(input,si+1,newoutput);
+        }
+}
+import java.util.Scanner;
+
+public class runner {
+	
+	public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] input = takeInput();
+		solution.printSubsets(input);
+	}
+}

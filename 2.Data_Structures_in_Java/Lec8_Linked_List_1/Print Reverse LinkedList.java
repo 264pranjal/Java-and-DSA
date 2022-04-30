71413a2830a88bab033b0b7c47406504a08b9c56
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-01 01:02:58 +0530

    Create Print Reverse LinkedList.java

---
+++ 2.Data_Structures_in_Java/Lec8_Linked_List_1/Print Reverse LinkedList.java
@@ -0,0 +1,108 @@
+/*
+You have been given a singly linked list of integers. Write a function to print the list in a reverse order.
+To explain it further, you need to start printing the data from the tail and move towards the head of the list, printing the head data at the end.
+Note :
+You can’t change any of the pointers in the linked list, just print it in the reverse order.
+ Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space.
+Remember/Constraints :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+Output format :
+For each test case, print the singly linked list of integers in a reverse fashion, in a row, separated by a single space.
+
+Output for every test case will be printed in a seperate line.
+ Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^3
+Time Limit: 1sec
+
+Where 'M' is the size of the singly linked list.
+Sample Input 1 :
+1
+1 2 3 4 5 -1
+Sample Output 1 :
+5 4 3 2 1
+Sample Input 2 :
+2
+1 2 3 -1
+10 20 30 40 50 -1
+Sample Output 2 :
+3 2 1
+50 40 30 20 10
+*/
+public class Solution {
+
+	public static void printReverse(LinkedListNode<Integer> root) {
+		if(root==null)
+            return;
+        printReverse(root.next);
+        System.out.print(root.data+" ");
+	}
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput(); 
+            Solution.printReverse(head);
+            System.out.println();
+            
+            t -= 1;
+
+        }
+        
+    }
+}

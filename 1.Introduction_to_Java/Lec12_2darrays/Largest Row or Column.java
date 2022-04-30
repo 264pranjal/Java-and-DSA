65544c9221f3ff1bd47010db4526744f4125ec4e
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-04-30 21:15:39 +0530

    Create Largest Row or Column.java

---
+++ 1.Introduction_to_Java/Lec12_2darrays/Largest Row or Column.java
@@ -0,0 +1,136 @@
+/*
+For a given two-dimensional integer array/list of size (N x M), you need to find out which row or column has the largest sum(sum of all the elements in a row/column) amongst all the rows and columns.
+Note :
+If there are more than one rows/columns with maximum sum, consider the row/column that comes first. And if ith row and jth column has the same largest sum, consider the ith row as answer.
+Input Format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains two integer values, 'N' and 'M', separated by a single space. They represent the 'rows' and 'columns' respectively, for the two-dimensional array/list.
+
+Second line onwards, the next 'N' lines or rows represent the ith row values.
+
+Each of the ith row constitutes 'M' column values separated by a single space.
+Output Format :
+For each test case, If row sum is maximum, then print: "row" <row_index> <row_sum>
+OR
+If column sum is maximum, then print: "column" <col_index> <col_sum>
+It will be printed in a single line separated by a single space between each piece of information.
+
+Output for every test case will be printed in a seperate line.
+ Consider :
+If there doesn't exist a sum at all then print "row 0 -2147483648", where -2147483648 or -2^31 is the smallest value for the range of Integer.
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^3
+0 <= M <= 10^3
+Time Limit: 1sec
+Sample Input 1 :
+1
+2 2 
+1 1 
+1 1
+Sample Output 1 :
+row 0 2
+Sample Input 2 :
+2
+3 3
+3 6 9 
+1 4 7 
+2 8 9
+4 2
+1 2
+90 100
+3 40
+-10 200
+Sample Output 2 :
+column 2 25
+column 1 342
+*/
+
+
+public class Solution {
+
+	public static void findLargest(int mat[][]){
+        int largest=Integer.MIN_VALUE;
+        int index=0;
+        boolean rowcol=false;
+        if(mat.length==0){
+            System.out.println("row "+index+" "+largest);
+            return;
+        }
+        for(int i=0;i<mat.length;i++){
+            int sum=0;
+            for(int j=0;j<mat[0].length;j++){
+                sum=sum+mat[i][j];
+            }
+            if(largest<sum){
+                largest=sum;
+                index=i;
+                rowcol=true;
+            }
+        }
+        for(int i=0;i<mat[0].length;i++){
+            int sum=0;
+            for(int j=0;j<mat.length;j++){
+                sum=sum+mat[j][i];
+            }
+            if(largest<sum){
+                largest=sum;
+                index=i;
+                rowcol=false;
+            }
+        }
+        if(rowcol==false)
+            System.out.println("column "+index+" "+largest);
+        else
+            System.out.println("row "+index+" "+largest);
+	}
+
+}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static int[][] take2DInput() throws IOException {
+        String[] strRowsCols = br.readLine().trim().split("\\s");
+        int n_rows = Integer.parseInt(strRowsCols[0]);
+        int m_cols = Integer.parseInt(strRowsCols[1]);
+
+        if (n_rows == 0) {
+            return new int[0][0];
+        }
+
+
+        int[][] mat = new int[n_rows][m_cols];
+
+        for (int row = 0; row < n_rows; row++) {
+            String[] strNums; 
+            strNums = br.readLine().trim().split("\\s");
+            
+            for (int col = 0; col < m_cols; col++) {
+                mat[row][col] = Integer.parseInt(strNums[col]);
+            }
+        }
+
+        return mat;
+    }
+
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[][] mat = take2DInput();
+
+            Solution.findLargest(mat);
+            System.out.println();
+
+            t -= 1;
+        }
+    }
+}

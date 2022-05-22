fa6bbed403dcd36224b1e5199d7f3fd27262750f
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-05-23 00:48:19 +0530

    Update Reverse String Wordwise.java

--- 1.Introduction_to_Java/Lec11_Strings/Reverse String Wordwise.java
+++ 1.Introduction_to_Java/Lec11_Strings/Reverse String Wordwise.java
@@ -18,34 +18,25 @@ code your indent Always
 */
 public class Solution {
 	public static String reverseWordWise(String input) {
-        int[][] dp = new int[s.length()][s.length()];
-        //find the logest palindromic subsequence 
-        for(int g = 0; g< s.length(); g++){
-            for(int i = 0, j = g; j < dp.length; i++, j++){
-                if(g == 0){
-                    dp[i][j] = 1;
-                }else if(g == 1){
-                    dp[i][j] = s.charAt(i) == s.charAt(j) ? 2 : 1;
-                }else{
-                    if(s.charAt(i) == s.charAt(j)){
-                        dp[i][j] = 2 + dp[i+1][j-1];
-                    }else{
-                        dp[i][j] = Math.max(dp[i][j-1] , dp[i+1][j]);
-                    }
-                }
+        String ans="";
+        int cws=0;
+        int i=0;
+        int n=input.length();
+        for(;i<n;i++){
+            if(input.charAt(i)==' '){
+                int cwe=i-1;
+                ans=input.substring(cws,cwe+1)+" "+ans;
+                cws=i+1;
+                
             }
         }
-        //ans == > length_of_string   -  longest_palindromic_subsequence 
-        int sublen = dp[0][s.length() - 1];
-        int len = s.length();
-        int change = len - sublen;
-        
-        if(change==0 || change ==1){
-            return 1;
-        }
-        return 0;
+        int cwe=i-1;
+        ans=input.substring(cws,cwe+1)+" "+ans;
+        return ans;
+	}
     
 }
+
 import java.util.Scanner;
 
 public class runner {

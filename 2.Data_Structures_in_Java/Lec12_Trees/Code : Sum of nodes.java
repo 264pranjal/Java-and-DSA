ad48d4e82bdc79a847416f0a92f2c50702128f90
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-06-03 20:42:25 +0530

    Create Code : Sum of nodes.java

---
+++ 2.Data_Structures_in_Java/Lec12_Trees/Code : Sum of nodes.java
@@ -0,0 +1,146 @@
+/*
+Given a generic tree, count and return the sum of all nodes present in the given tree.
+Input format :
+Elements in level order form separated by space (as per done in class). Order is - 
+Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element 
+Output Format :
+Sum of all nodes
+Sample Input :
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output :
+190
+*/
+
+public class Solution {
+
+	
+	public static int sumOfAllNode(TreeNode<Integer> root){
+        if(root==null)
+            return 0;
+        int count=root.data;
+        for(int i=0;i<root.children.size();i++){
+            count=count+sumOfAllNode(root.children.get(i));
+        }
+        return count;
+	
+	}
+}
+
+//MAIN CODE
+import java.util.ArrayList;
+import java.util.Scanner;
+
+ class QueueEmptyException extends Exception {
+	 
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+   class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}
+ 
+public class Main {
+
+	
+
+	static Scanner s = new Scanner(System.in);
+
+	public static TreeNode<Integer> takeInputLevelWise(){
+		QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<TreeNode<Integer>>();  // Queue of node that are entered themselves but their children aren't added yet
+		int rootData = s.nextInt();
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		while(!pendingNodes.isEmpty()){
+			TreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+				int numChild = s.nextInt();
+				for(int i = 0 ; i < numChild; i++){
+					int currentChild = s.nextInt();
+					TreeNode<Integer> childNode = new TreeNode<Integer>(currentChild);
+					pendingNodes.enqueue(childNode);
+					currentNode.children.add(childNode);
+				}
+			} catch (QueueEmptyException e) {
+			}
+		}
+		return root;
+	}
+
+
+	public static void main(String[] args) {
+		TreeNode<Integer> root =  takeInputLevelWise();
+		System.out.println(Solution.sumOfAllNode(root));
+	}
+
+}

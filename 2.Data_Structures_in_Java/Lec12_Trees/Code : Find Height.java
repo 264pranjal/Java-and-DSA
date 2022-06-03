37a6eeb4cd280b239999c2f29702e98904bc6202
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-06-03 22:20:21 +0530

    Create Code : Find Height.java

---
+++ 2.Data_Structures_in_Java/Lec12_Trees/Code : Find Height.java
@@ -0,0 +1,155 @@
+/*
+Given a generic tree, find and return the height of given tree.
+Input Format:
+The first line of input contains data of the nodes of the tree in level order form. The order is: data for root node, number of children to root node, data of each of child nodes and so on and so forth for each node. The data of the nodes of the tree is separated by space.  
+Output Format :
+The first and only line of output prints the height of the given generic tree.
+Constraints:
+Time Limit: 1 sec
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1:
+3
+*/
+
+public class Solution {
+
+	public static int getHeight(TreeNode<Integer> root){
+        if(root==null)
+            return 0;
+        int max=0;
+        for(int i=0;i<root.children.size();i++){
+            int h= getHeight(root.children.get(i));
+            if(h>max)
+                max=h;
+        }
+        return max+1;
+	}
+
+}
+
+//Main code
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+
+		Node(T data) {
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException {
+		if (size == 0) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+	public void enqueue(T element) {
+		Node<T> newNode = new Node<T>(element);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException {
+		if (head == null) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if (head == tail) {
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class TreeNode<T> {
+	T data;
+	ArrayList<TreeNode<T>> children;
+
+	TreeNode(T data) {
+		this.data = data;
+		children = new ArrayList<TreeNode<T>>();
+	}
+}
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	static StringTokenizer st;
+
+	public static TreeNode<Integer> takeInputLevelWise() throws IOException {
+		QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<TreeNode<Integer>>(); // Queue of node that are
+		// entered themselves
+		// but their children
+		// aren't added yet
+		st = new StringTokenizer(br.readLine());
+		int rootData = Integer.parseInt(st.nextToken());
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		while (!pendingNodes.isEmpty()) {
+			TreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+				int numChild = Integer.parseInt(st.nextToken());
+				for (int i = 0; i < numChild; i++) {
+					int currentChild = Integer.parseInt(st.nextToken());
+					TreeNode<Integer> childNode = new TreeNode<Integer>(currentChild);
+					pendingNodes.enqueue(childNode);
+					currentNode.children.add(childNode);
+				}
+			} catch (QueueEmptyException e) {
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) throws IOException {
+		TreeNode<Integer> root = takeInputLevelWise();
+		System.out.println(Solution.getHeight(root));
+
+	}
+
+}
+*/

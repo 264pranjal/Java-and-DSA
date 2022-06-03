65dd4e2d4fd0bcd512ebde7d8d51523a0c7b0952
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-06-03 22:51:44 +0530

    Update Code : Pre-order Traversal.java

--- 2.Data_Structures_in_Java/Lec12_Trees/Code : Pre-order Traversal.java
+++ 2.Data_Structures_in_Java/Lec12_Trees/Code : Pre-order Traversal.java
@@ -3,7 +3,7 @@
         	return;
         System.out.print(root.data+" ");
         for(int i=0;i<root.children.size();i++){
-        	printPostOrder(root.children.get(i));
+        	printPreOrder(root.children.get(i));
         }
         return;
 	}

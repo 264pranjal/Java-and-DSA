316e32f972fe4a8cd791f822505e4acd2a58ab93
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-06-06 23:02:58 +0530

    Update Check if a Binary Tree is BST.java

--- 2.Data_Structures_in_Java/Lec15_BST1/Check if a Binary Tree is BST.java
+++ 2.Data_Structures_in_Java/Lec15_BST1/Check if a Binary Tree is BST.java
@@ -16,6 +16,63 @@ Sample Input 2 :
 Sample Output 2 :
 false
 */
+//3rd way
+public class Solution {
+
+	public static boolean isBST(BinaryTreeNode<Integer> root) {
+        return isBST2(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+	}
+    public static boolean isBST2(BinaryTreeNode<Integer> root,int min,int max){
+        if(root==null)
+            return true;
+        if(root.data<min || root.data>max)
+            return false;
+        boolean isleftok=isBST2(root.left,min,root.data-1);
+        boolean isrightok=isBST2(root.right,root.data,max);
+        return isleftok && isrightok;
+    }
+
+}
+
+/*
+2nd way
+
+class isBSTcheck{
+    public boolean isBST;
+    public int minimum;
+    public int maximum;
+}
+public class Solution {
+
+	public static boolean isBST(BinaryTreeNode<Integer> root) {
+        isBSTcheck output = isBST3(root);
+        return output.isBST;
+	}
+    public static isBSTcheck isBST3(BinaryTreeNode<Integer> root){
+        if(root==null){
+            isBSTcheck output = new isBSTcheck();
+            output.isBST=true;
+            output.minimum=Integer.MAX_VALUE;
+            output.maximum=Integer.MIN_VALUE;
+            return output;
+        }
+        isBSTcheck leftok=isBST3(root.left);
+        isBSTcheck rightok=isBST3(root.right);
+        int mini=Math.min(root.data,Math.min(leftok.minimum,rightok.minimum));
+        int maxi=Math.max(root.data,Math.max(leftok.maximum,rightok.maximum));
+        boolean isBSTfinal=(root.data > leftok.maximum) && (root.data<=rightok.minimum) && leftok.isBST && rightok.isBST;
+        isBSTcheck output = new isBSTcheck();
+        output.isBST=isBSTfinal;
+        output.minimum=mini;
+        output.maximum=maxi;
+        return output;
+    }
+
+}
+*/
+
+/*
+1st way
 public class Solution {
 
 	public static boolean isBST(BinaryTreeNode<Integer> root) {
@@ -42,3 +99,4 @@ public class Solution {
     }    
 
 }
+*/

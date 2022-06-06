4ce9c73b57ec0fd4a1483651d4891f3eba189c7e
Pranjal Agrawal <37728750+264pranjal@users.noreply.github.com>
2022-06-06 23:39:05 +0530

    Update Code: BST to Sorted LL.java

--- 2.Data_Structures_in_Java/Lec15_BST1/Code: BST to Sorted LL.java
+++ 2.Data_Structures_in_Java/Lec15_BST1/Code: BST to Sorted LL.java
@@ -11,3 +11,114 @@ Sample Input 1:
 Sample Output 1:
 2 5 6 7 8 10
 */
+class PassNode<T>
+{
+	T head;
+    T tail;
+}
+public class Solution {
+	public static LinkedListNode<Integer> constructLinkedList(BinaryTreeNode<Integer> root) {
+        PassNode<LinkedListNode<Integer>> passNode= constructLinkedListHelper(root);
+        return passNode.head;
+	}
+    
+    public static PassNode<LinkedListNode<Integer>> constructLinkedListHelper(BinaryTreeNode<Integer> root)
+    {
+    	if(root==null)
+        {
+            return new PassNode<LinkedListNode<Integer>>();
+        }
+        
+        if (root.left==null && root.right==null)
+        {
+        	PassNode<LinkedListNode<Integer>> passNode = new PassNode<LinkedListNode<Integer>>();
+        	passNode.head=new LinkedListNode<Integer>(root.data);
+            passNode.tail=null;
+            return passNode;
+        }
+        
+        PassNode<LinkedListNode<Integer>> passNodeLeft = constructLinkedListHelper(root.left);
+        PassNode<LinkedListNode<Integer>> passNodeRight = constructLinkedListHelper(root.right);
+        PassNode<LinkedListNode<Integer>> passNode = new PassNode<LinkedListNode<Integer>>();
+        LinkedListNode<Integer> newNode=new LinkedListNode<Integer>(root.data);
+        
+        if (passNodeLeft.head==null)
+        {
+        	passNode.head=newNode;
+        }
+        else
+        {
+        	passNode.head=passNodeLeft.head;
+        }
+        
+        if(passNodeLeft.tail==null)
+        {
+        	passNode.head.next=newNode;
+            passNode.tail=newNode;
+        }
+        else
+        {
+        	passNodeLeft.tail.next=newNode;
+            passNode.tail=newNode;
+        }
+        
+        if (passNodeRight.head!=null)
+        {
+        	passNode.tail.next=passNodeRight.head;
+            passNode.tail=passNodeRight.head;
+        }
+        
+        if(passNodeRight.tail!=null)
+        {
+        	passNode.tail=passNodeRight.tail;
+        }
+    
+        return passNode;
+   
+    }
+}
+/*
+import java.util.*;
+public class Solution {
+	public static LinkedListNode<Integer> constructLinkedList(BinaryTreeNode<Integer> root) {
+        if(root==null){
+            LinkedListNode<Integer> head=null;
+            return head;
+        }
+        ArrayList<Integer> ans=new ArrayList();
+        ans=construct(root,ans);
+        LinkedListNode<Integer> head=null;
+        for(int i=0;i<ans.size();i++){
+            head=insert(head,ans.get(i));
+        }
+        return head;
+        
+	}
+    public static LinkedListNode<Integer> insert(LinkedListNode<Integer> root, int item)
+    {
+        LinkedListNode<Integer> temp = new LinkedListNode<Integer>(0);
+        LinkedListNode<Integer> ptr=null;
+        temp.data = item;
+        temp.next = null;
+
+        if (root == null)
+            root = temp;
+        else
+        {
+            ptr = root;
+            while (ptr.next != null)
+                ptr = ptr.next;
+            ptr.next = temp;
+        }
+        return root;
+    }
+    public static ArrayList<Integer> construct(BinaryTreeNode<Integer> root,ArrayList<Integer> ans){
+        if(root==null)
+            return ans;
+        ans=construct(root.left,ans);
+        ans.add(root.data);
+        ans=construct(root.right,ans);
+        return ans;
+    }
+}
+*/
